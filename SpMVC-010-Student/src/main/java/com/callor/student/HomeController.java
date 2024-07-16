2a9cec699c2d7ba83844fcb84d44061ddf58dbda
inhoo123 <xkdns50@naver.com>
2024-07-16 11:49:39 +0900

    setting

---
+++ SpMVC-010-Student/src/main/java/com/callor/student/HomeController.java
@@ -0,0 +1,36 @@
+package com.callor.student;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+    //http://localhost:8080/student/로 request 가 오면
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
