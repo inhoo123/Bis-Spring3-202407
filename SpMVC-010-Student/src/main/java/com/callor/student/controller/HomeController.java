b08b66516a4e16446be849238a755400f3637d92
inhoo123 <xkdns50@naver.com>
2024-07-17 13:53:41 +0900

    asdf

--- SpMVC-010-Student/src/main/java/com/callor/student/controller/HomeController.java
+++ SpMVC-010-Student/src/main/java/com/callor/student/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.callor.student.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
@@ -10,35 +11,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.callor.student.models.StudentVO;
+import com.callor.student.persistance.StudentDao;
 
 @Controller
 public class HomeController {
+
+	private final StudentDao studentDao;
+
+	public HomeController(StudentDao studentDao) {
+		super();
+		this.studentDao = studentDao;
+	}
 
 	// http://localhost:8080/student/ 로 request 가 오면
 	// http://localhost:8080/student 로 request 가 오면
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		List<StudentVO> stList = studentDao.selectAll();
 
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		String formattedDate = dateFormat.format(date);
-		model.addAttribute("serverTime", formattedDate);
+		model.addAttribute("ST_LIST", stList);
 		return "home";
 	}
 
-
-	
 	@RequestMapping(value = "/insert", method = RequestMethod.GET)
 	public String insert() {
 		return "student/input";
 	}
 
-
 	// 학생정보를 입력받기 위한 화면을 보여주는 method
 	@RequestMapping(value = "/insert", method = RequestMethod.POST)
 	public String insert(StudentVO studentVO, Model model) {
 		model.addAttribute("STD", studentVO);
-		return "student/view";
+
+		int result = studentDao.insert(studentVO);
+//		List<StudentVO> stList = studentDao.selectAll();
+//		model.addAttribute("ST_LIST", stList);
+//		return "home";
+		return "redirect:/";
 	}
 
 }
--- SpMVC-010-Student/src/main/java/com/callor/student/persistance/StudentDao.java
+++ SpMVC-010-Student/src/main/java/com/callor/student/persistance/StudentDao.java
@@ -1,10 +1,18 @@
 package com.callor.student.persistance;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import com.callor.student.models.StudentVO;
 
 public interface StudentDao {
-	@Insert("")
+	@Select("Select * FROM tbl_student ORDER BY st_num")
+	public List<StudentVO> selectAll();
+	
+	@Insert("INSERT  INTO tbl_student ( st_num,st_name,st_dept,st_grade,st_tel,st_addr)"
+			+ "VALUES(#{st_num},#{st_name},#{st_dept},#{st_grade},#{st_tel},#{st_addr})")
+	
 	public int insert(StudentVO stVO);
 }
