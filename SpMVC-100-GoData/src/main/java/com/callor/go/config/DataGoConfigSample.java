36131a24637aa8d04e3562152cc29f183803ff60
inhoo123 <xkdns50@naver.com>
2024-08-12 15:31:59 +0900

    공공데이터터

--- SpMVC-100-GoData/src/main/java/com/callor/go/config/DataGoConfigSample.java
+++ SpMVC-100-GoData/src/main/java/com/callor/go/config/DataGoConfigSample.java
@@ -1,5 +1,10 @@
 package com.callor.go.config;
 
 public class DataGoConfigSample {
-
+	/*
+	 * 공공데이터에서 API 키를 발급받고, GO_API_KEY 값으로 설정한 후 
+	 * DAtaGoConfigSample 파일을
+	 */
+	public final static String GO_API_KEY = "YOUR API KEY";
+	public final static String BIS_URL ="http://api.gwangju.go.kr/xml/stationInfo";
 }
